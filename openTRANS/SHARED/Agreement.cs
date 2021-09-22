67c182a681a526e6bc67b878f22ea4ca357fd27d
Lukas Gasselsberger | alu-one <l.gasselsberger@alu-one.at>
2021-09-22 16:18:27 +0200

    Resolve conflicts

--- openTRANS/SHARED/Agreement.cs
+++ openTRANS/SHARED/Agreement.cs
@@ -1,9 +1,13 @@
-﻿using System.Xml.Serialization;
+﻿using System;
+using System.Xml.Serialization;
 
 namespace openTRANS {
     public partial class Agreement
     {
         [XmlElement("AGREEMENT_ID")]
         public string AgreementId;
+
+        [XmlElement("AGREEMENT_END_DATE")]
+        public DateTime AgreementEndDate;
     }
 }
